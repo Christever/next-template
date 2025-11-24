52ab846d587bc1d19459918a9004136dcdb787d7
Christever <christian@steverlynck.fr>
2025-11-24 18:07:22 +0100

    lucide + daisy-ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,14 @@
+import { Eye } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
-      <p>Acceuil</p>
+    <div className="flex min-h-screen justify-center mt-5">
+     
+        <button className="btn">
+        <Eye size={24}/>
+        Click me
+        </button>
+    
     </div>
   );
 }
